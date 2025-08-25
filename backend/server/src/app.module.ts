c9029cbac54bf8d926798c508d6745e5234b417a
hafizhuzaifaahmed <huzaifaahmed90000@gmail.com>
2025-08-25 13:49:21 +0500

    feat/people-module, remove the issue which was causing merge conflict

--- backend/server/src/app.module.ts
+++ backend/server/src/app.module.ts
@@ -10,11 +10,10 @@ import { TableModule } from './table/table.module';
 import { RenamedfunctionModule } from './renamedfunction/renamedfunction.module';
 import { JobModule } from './job/job.module';
 import { ProcessModule } from './process/process.module';
-import { TaskModule } from './task/task.module';
 import { PeopleModule } from './people/people.module';
 
 @Module({
-  imports: [PrismaModule, CompanyModule, BuildingModule, FloorModule, RoomModule, TableModule, RenamedfunctionModule, JobModule, ProcessModule, TaskModule, PeopleModule],
+  imports: [PrismaModule, CompanyModule, BuildingModule, FloorModule, RoomModule, TableModule, RenamedfunctionModule, JobModule, ProcessModule, PeopleModule],
   controllers: [AppController],
   providers: [AppService],
 })
